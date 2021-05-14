7c5254313d1a69b223b52b90629fd77d304638b7
manvantar <manukvshetty@gmail.com>
2021-05-14 18:35:07 +0530

    Added: Addressbook with Name,City,State,Pincode,Phonenumber,email

---
+++ js/AddressBook.js
@@ -0,0 +1,18 @@
+class Contact{
+
+    constructor(param) {
+        this.firstName = param[0];
+        this.lastName = param[1];
+        this.city = param[2];
+        this.state = param[3];
+        this.pincode = param[4];
+        this.phoneNumber = param[5];
+        this.email = param[6];
+        }
+    }
+    console.log('Welcome to Address book');
+
+    let contact=new Array("Manu","K V","Bengaluru","Karnataka",560076,9663393660,"manukvhetty@gmail.com")
+    contact1=new Contact(contact);
+    console.log(contact1)
+    console.log('Welcome to address book ')
